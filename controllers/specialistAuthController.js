1cee58487559d9fc1530343fb195335e5c2bac26
Chris Jindu <chrisjindu04@gmail.com>
2025-11-12 01:15:31 -0800

    Update specialistAuthController.js

--- controllers/specialistAuthController.js
+++ controllers/specialistAuthController.js
@@ -43,7 +43,7 @@ export const specialistRegister = async (req, res) => {
       specialty,
       phone,
       bio,
-      accountStatus: process.env.AUTO_APPROVE === 'true' ? 'approved' : 'pending', // Requires admin approval
+      accountStatus: 'pending', // Requires admin approval
       lastLogin: new Date()
     });
 
