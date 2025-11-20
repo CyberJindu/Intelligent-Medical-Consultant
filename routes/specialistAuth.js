91ea418885acba150347fc92cd2d77da69ed4ada
Chris Jindu <chrisjindu04@gmail.com>
2025-11-20 06:17:24 -0800

    Update specialistAuth.js

--- routes/specialistAuth.js
+++ routes/specialistAuth.js
@@ -2,7 +2,8 @@ import express from 'express';
 import { 
   specialistRegister, 
   specialistLogin, 
-  getSpecialistProfile 
+  getSpecialistProfile, 
+  updateSpecialistProfile
 } from '../controllers/specialistAuthController.js';
 import { specialistAuthMiddleware } from '../middleware/specialistAuth.js';
 
@@ -14,5 +15,6 @@ router.post('/login', specialistLogin);
 
 // Protected routes (require specialist authentication)
 router.get('/profile', specialistAuthMiddleware, getSpecialistProfile);
+router.put('/profile', specialistAuthMiddleware, updateSpecialistProfile);
 
 export default router;
