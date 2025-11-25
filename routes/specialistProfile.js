59388db6be9bde13112f540f1870ec07049bfe90
Chris Jindu <chrisjindu04@gmail.com>
2025-11-25 01:40:28 -0800

    Update specialistProfile.js

--- routes/specialistProfile.js
+++ routes/specialistProfile.js
@@ -1,1 +1,19 @@
+import express from 'express';
+import { 
+  updateSpecialistProfile, 
+  deleteProfilePicture 
+} from '../controllers/specialistProfileController.js';
+import { specialistAuthMiddleware } from '../middleware/specialistAuth.js';
 
+const router = express.Router();
+
+// All routes require specialist authentication
+router.use(specialistAuthMiddleware);
+
+// Update profile (with image upload)
+router.put('/profile', updateSpecialistProfile);
+
+// Delete profile picture
+router.delete('/profile/picture', deleteProfilePicture);
+
+export default router;
