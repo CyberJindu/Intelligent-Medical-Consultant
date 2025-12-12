a8e91dd0b56c5b6b4053578428d67e39eadf10cc
Chris Jindu <chrisjindu04@gmail.com>
2025-12-12 01:07:00 -0800

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -3,7 +3,8 @@ import {
   sendMessage, 
   getConversations, 
   getConversation, 
-  deleteConversation 
+  deleteConversation,
+  sendMessageWithImage  // Add this import
 } from '../controllers/chatController.js';
 import { authMiddleware } from '../middleware/auth.js';
 import { validateSendMessage } from '../middleware/validation.js';
@@ -15,6 +16,9 @@ router.use(authMiddleware);
 
 // Send message to AI
 router.post('/send', validateSendMessage, sendMessage);
+
+// NEW: Send message with image
+router.post('/send-image', sendMessageWithImage);
 
 // Get user's conversation list
 router.get('/conversations', getConversations);
